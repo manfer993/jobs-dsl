3903687b1dc2d48c8421401e992206178b91ec41
manfer993 <manfer93@gmail.com>
2019-04-15 11:39:25 -0500

    fix file

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS') {
     scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigName('manfer993')
+            node / gitConfigEmail('manfer93@gmail.com')
         }
     }
     triggers {
