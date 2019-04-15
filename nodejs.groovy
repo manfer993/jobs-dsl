be97c1a0a0896a5a79c431fd3a2ab9ede1dbc52e
manfer993 <manfer93@gmail.com>
2019-04-15 14:55:17 -0500

    borrar sonar

--- nodejs.groovy
+++ nodejs.groovy
@@ -13,20 +13,20 @@ job('Build_Nodejs_Project') {
         nodejs('nodeJS_10.15.3') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Global Tools -> NodeJS Installations -> Name
     }
-    steps {
-        shell("npm install @angular/cli")
-        shell("npm install")
-        sonarScanner properties:
-        '''
-        sonar.projectKey= build_NodeJs_App
-        sonar.projectName= build_NodeJs_App
-        sonar.projectVersion=1.0
-        sonar.sourceEncoding=UTF-8
-        sonar.sources=src
-        sonar.exclusions=**/node_modules/**,**/*.spec.ts,**/dist/**,**/docs/**,**/*.js,**/e2e/**,**/coverage/**
-        sonar.tests=src/app
-        sonar.test.inclusions=**/*.spec.ts
-        sonar.ts.tslint.configPath=tslint.json
-        '''        
-    }
+    // steps {
+    //     shell("npm install @angular/cli")
+    //     shell("npm install")
+    //     sonarScanner properties:
+    //     '''
+    //     sonar.projectKey= build_NodeJs_App
+    //     sonar.projectName= build_NodeJs_App
+    //     sonar.projectVersion=1.0
+    //     sonar.sourceEncoding=UTF-8
+    //     sonar.sources=src
+    //     sonar.exclusions=**/node_modules/**,**/*.spec.ts,**/dist/**,**/docs/**,**/*.js,**/e2e/**,**/coverage/**
+    //     sonar.tests=src/app
+    //     sonar.test.inclusions=**/*.spec.ts
+    //     sonar.ts.tslint.configPath=tslint.json
+    //     '''        
+    // }
 }
