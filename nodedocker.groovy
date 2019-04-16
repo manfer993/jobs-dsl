3c3dc2c8a8660d197b0b0665185964a8d13fc30c
manfer993 <manfer93@gmail.com>
2019-04-16 09:51:57 -0500

    add sleep

--- nodedocker.groovy
+++ nodedocker.groovy
@@ -28,7 +28,7 @@ job('Build_Nodejs_Project_docker2') {
         shell('if [[ -n `docker container ls -a | grep portafolioDev` ]]; \
         then docker container stop portafolioDev && docker container rm portafolioDev; \
         fi')
-        shell(sleep 5)  
+        shell('sleep 5')  
         shell('docker run -d -p 8085:80 --name portafolioDev ferman18/nodejs_app_dev:${BUILD_NUMBER}')      
     }
 }
