1f8b8c98750cb2c7266ddb9f797309b9a3b54d2f
manfer993 <manfer93@gmail.com>
2019-04-16 09:06:15 -0500

    fix credentials

--- nodedocker.groovy
+++ nodedocker.groovy
@@ -10,16 +10,16 @@ job('Build_Nodejs_Project_docker2') {
         scm('H/5 * * * *')
     }
     wrappers {
-        nodejs('nodeJS_10.15.3') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Global Tools -> NodeJS Installations -> Name
+        nodejs('nodeJS_10.15.3')    // this is the name of the NodeJS installation in 
+                                    // Manage Jenkins -> Configure Global Tools -> NodeJS Installations -> Name
     }
     steps {
-        shell("npm install @angular/cli")
-        shell("npm install")
+        shell('npm install @angular/cli')
+        shell('npm install')
         dockerBuildAndPublish {
             repositoryName('ferman18/nodejs_app_dev')
             tag('${BUILD_NUMBER}')
-            registryCredentials('dockerhub')
+            registryCredentials('dockerHub')
             forcePull(false)
             forceTag(false)
             createFingerprints(false)
