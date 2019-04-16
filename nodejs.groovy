b399afed90ade41ceebdad7249a6e903389ae1ff
manfer993 <manfer93@gmail.com>
2019-04-16 13:31:57 -0500

    clean project

--- nodejs.groovy
+++ nodejs.groovy
@@ -1,9 +1,5 @@
 job('deploy-app-heroku') {
     scm {
-        // git('https://github.com/manfer993/portafolio.git','development'){ node -> // is hudson.plugins.git.GitSCM
-        //     node / gitConfigName('manfer993')
-        //     node / gitConfigEmail('manfer93@gmail.com')
-        // }
         git(){ 
             remote {
                 name('origin')
@@ -24,22 +20,13 @@ job('deploy-app-heroku') {
         }
     }
     triggers {
-        githubPush()
-        // scm('H/5 * * * *')
+        githubPush()        
     }
-    // wrappers {
-    //     nodejs('nodeJS_10.15.3') // this is the name of the NodeJS installation in 
-    //                      // Manage Jenkins -> Configure Global Tools -> NodeJS Installations -> Name
-    // }
-    // steps {
-    //     shell("npm install @angular/cli")
-    //     shell("npm install")
-    // }
     publishers {
         git {
             pushOnlyIfSuccess()
-            tag('heroku', 'foo-$PIPELINE_VERSION') {
-                message('Release $PIPELINE_VERSION')
+            tag('heroku', 'foo-${BUILD_NUMBER}') {
+                message('Release number ${BUILD_NUMBER}')
                 create()
             }
             branch('heroku', 'master')
