bf0c08684ebc64010e3725079f29e7bd59e19df0
manfer993 <manfer93@gmail.com>
2019-04-17 10:51:57 -0500

    fix brach name

--- pipelineNodejsApp.groovy
+++ pipelineNodejsApp.groovy
@@ -12,7 +12,7 @@ pipelineJob('Portafolio_deploy_docker') {
                         name('origin')
                         url('https://github.com/manfer993/portafolio.git') 
                     }
-                    branches('*/developmentmaster')
+                    branches('*/development')
                     scriptPath('jenkinsfile')
                     extensions { }  // required as otherwise it may try to tag the repo, which you may not want
                 }
